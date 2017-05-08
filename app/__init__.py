6a55baf826b5f81c3210a43f4e193377c8ce59d6
molo76 <molo976@gmail.com>
2017-05-08 22:33:00 +0100

    added SQLAlchemy & base Flask config

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,23 @@
 # app/__init__.py
 
+# third-party imports
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
-# Initialize the app
-app = Flask(__name__, instance_relative_config=True)
+# local imports
+from config import app_config
 
-# Load the views
-from app import views
+# db variable initialization
+db = SQLAlchemy()
 
-# Load the config file
-app.config.from_object('config')
+def create_app(config_name):
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_object(app_config[config_name])
+    app.config.from_pyfile('config.py')
+    db.init_app(app)
+
+    # temporary route
+    @app.route('/')
+    def hello_world():
+        return 'Hello, World!'
+    return app
--- config.py
+++ config.py
@@ -2,4 +2,29 @@
 
 # Enable Flask's debugging features. Should be false in production.
 
-DEBUG = True
+class Config(object):
+    """
+    Common configurations
+    """
+
+    # Put any configurations here that are common across all environments
+
+class DevelopmentConfig(Config):
+    """
+    Development configurations
+    """
+
+    DEBUG = True
+    SQLALCHEMY_ECHO = True
+
+class ProductionConfig(Config):
+    """
+    Production configurations
+    """
+
+    DEBUG = False
+
+app_config = {
+    'development': DevelopmentConfig,
+    'production': ProductionConfig
+}
