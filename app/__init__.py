3ed7375bae5106269796e3391019beae0740dc3d
molo76 <molo976@gmail.com>
2017-05-09 22:43:11 +0100

    home blueprint done

--- app/__init__.py
+++ app/__init__.py
@@ -3,21 +3,39 @@
 # third-party imports
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_migrate import Migrate
 
 # local imports
 from config import app_config
 
 # db variable initialization
 db = SQLAlchemy()
+login_manager = LoginManager()
 
 def create_app(config_name):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_object(app_config[config_name])
     app.config.from_pyfile('config.py')
     db.init_app(app)
+    login_manager.init_app(app)
+    login_manager.login_message = "You must be logged in to access this page."
+    login_manager.login_vew = "auth.login"
+    migrate = Migrate(app, db)
+
+    from app import models
+
+    from .admin import admin as admin_blueprint
+    app.register_blueprint(admin_blueprint, url_prefix='/admin')
+
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint)
+
+    from .home import home as home_blueprint
+    app.register_blueprint(home_blueprint)
 
     # temporary route
-    @app.route('/')
-    def hello_world():
-        return 'Hello, World!'
+    #@app.route('/')
+    #def hello_world():
+    #    return 'Hello, World!'
     return app
